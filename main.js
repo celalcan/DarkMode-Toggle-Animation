9b7efea648b576e59e641f0453751fad7f5c567b
celalcan <celalcanh.h.oglu@gmail.com>
2022-06-26 00:04:31 +0300

    güncelleme

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
-var framegenislik = document.querySelector("img").offsetWidth;
-var frameyukseklik = document.querySelector("img").offsetHeight;
 
-console.log(framegenislik, frameyukseklik);
-window.onscroll = function(){
-
+   let framegenislik = document.querySelector("img").offsetWidth;
+   let frameyukseklik = document.querySelector("img").offsetHeight;
     document.querySelector(".mobile-frame").style.height = frameyukseklik + "px";
+     document.querySelector(".mobile-frame").style.width =framegenislik + "px";
+     document.querySelector(".frame-mobil").style.height = frameyukseklik + "px";
+     document.querySelector(".frame-mobil").style.width =framegenislik + "px";
+ 
+window.addEventListener('resize', function(event){
+    let framegenislik = document.querySelector("img").offsetWidth;
+  let frameyukseklik = document.querySelector("img").offsetHeight;
+   document.querySelector(".mobile-frame").style.height = frameyukseklik + "px";
     document.querySelector(".mobile-frame").style.width =framegenislik + "px";
-}
+    document.querySelector(".frame-mobil").style.height = frameyukseklik + "px";
+    document.querySelector(".frame-mobil").style.width =framegenislik + "px";
+});
