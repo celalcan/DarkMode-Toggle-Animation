8959c0100fffff3a5557fbbbcb4b21e36fe4b860
celalcan <celalcanh.h.oglu@gmail.com>
2022-06-26 21:46:25 +0300

    güncelleme

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 
-   let framegenislik = document.querySelector("img").offsetWidth;
-   let frameyukseklik = document.querySelector("img").offsetHeight;
+   let framegenislik = document.querySelector("#rect").offsetWidth;
+   let frameyukseklik = document.querySelector("#rect").offsetHeight;
+   console.log(framegenislik, frameyukseklik);
     document.querySelector(".mobile-frame").style.height = frameyukseklik + "px";
      document.querySelector(".mobile-frame").style.width =framegenislik + "px";
      document.querySelector(".frame-mobil").style.height = frameyukseklik + "px";
      document.querySelector(".frame-mobil").style.width =framegenislik + "px";
  
 window.addEventListener('resize', function(event){
-    let framegenislik = document.querySelector("img").offsetWidth;
-  let frameyukseklik = document.querySelector("img").offsetHeight;
+    let framegenislik = document.querySelector("#rect").offsetWidth;
+    let frameyukseklik = document.querySelector("#rect").offsetHeight;
    document.querySelector(".mobile-frame").style.height = frameyukseklik + "px";
     document.querySelector(".mobile-frame").style.width =framegenislik + "px";
     document.querySelector(".frame-mobil").style.height = frameyukseklik + "px";
