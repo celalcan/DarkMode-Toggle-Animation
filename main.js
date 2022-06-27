3c276a7bcb71265103d2dd21fe191d761fca8acc
celalcan <celalcanh.h.oglu@gmail.com>
2022-06-27 15:37:23 +0300

    güncelleme

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 
-   let framegenislik = document.querySelector("#rect").offsetWidth;
+   function myFunction(){
+    let framegenislik = document.querySelector("#rect").offsetWidth;
    let frameyukseklik = document.querySelector("#rect").offsetHeight;
    console.log(framegenislik, frameyukseklik);
     document.querySelector(".mobile-frame").style.height = frameyukseklik + "px";
      document.querySelector(".mobile-frame").style.width =framegenislik + "px";
-     document.querySelector(".frame-mobil").style.height = frameyukseklik + "px";
-     document.querySelector(".frame-mobil").style.width =framegenislik + "px";
+   }
+   
+     /* document.querySelector(".frame-mobil").style.height = frameyukseklik + "px";
+     document.querySelector(".frame-mobil").style.width =framegenislik + "px"; */
  
 window.addEventListener('resize', function(event){
     let framegenislik = document.querySelector("#rect").offsetWidth;
