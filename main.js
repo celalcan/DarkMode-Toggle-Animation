7879f2c779b63d2b2e813c812bee4a6d9a661774
celalcan <celalcanh.h.oglu@gmail.com>
2022-06-29 00:51:40 +0300

    güncelleme

--- main.js
+++ main.js
@@ -6,6 +6,19 @@
     document.querySelector(".mobile-frame").style.height = frameyukseklik + "px";
      document.querySelector(".mobile-frame").style.width =framegenislik + "px";
    }
+   let kayma=0;
+   function myFunction2(){
+   kaymaf();
+    
+    document.querySelector(".mode-circle").style.marginRight = "0px";
+   }
+   function kaymaf(){
+    kayma+=0.15;
+    if(kayma<2.638){
+    setTimeout(function(){kaymaf()},8);       
+     } 
+     document.querySelector(".mode-circle").style.transform = "translateX(-"+kayma+"vw)";
+}
    
      /* document.querySelector(".frame-mobil").style.height = frameyukseklik + "px";
      document.querySelector(".frame-mobil").style.width =framegenislik + "px"; */
