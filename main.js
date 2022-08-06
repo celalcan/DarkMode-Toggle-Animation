c3ae8275bc6cbff5bcfdddb557f367e837b3639e
celalcan <celalcanh.h.oglu@gmail.com>
2022-08-07 02:26:10 +0300

    güncelleme

--- main.js
+++ main.js
@@ -44,6 +44,20 @@
      document.querySelector(".mode-circle").style.background = "#ffff";
      document.querySelector(".mode-circle-ft").style.Color = "#ffff";
      document.querySelector(".mobile-frame").style.background= "#ffff";
+     document.querySelector(".alt-box").style.background= "#eee";
+     const nodeList = document.querySelectorAll(".alt-box-1");
+for (let i = 0; i < nodeList.length; i++) {
+  nodeList[i].style.color = "#C0C0C0";
+}
+     const nodeList2 = document.querySelectorAll(".alt-box-2");
+for (let i = 0; i < nodeList2.length; i++) {
+  nodeList2[i].style.color = "#5F5F5F";
+}
+     const nodeList3 = document.querySelectorAll(".alt-box-3-yazi");
+for (let i = 0; i < nodeList3.length; i++) {
+  nodeList3[i].style.color = "#000";
+}
+     document.querySelector(".alt-boxa").style.background= "#eee";
      document.querySelector(".moon").style.transform = "rotate(0deg)";
      document.querySelector(".sun").style.transform = "rotate(260deg)";
      document.querySelector(".yazi").style.color = "black";
@@ -70,6 +84,20 @@
      document.querySelector(".mode-circle").style.marginRight = "0.26vw";
      document.querySelector(".mode-circle").style.background= "#D9D9D9";
      document.querySelector(".mobile-frame").style.background= "black";
+     document.querySelector(".alt-box").style.background= "#1B1A1D";
+     const nodeList = document.querySelectorAll(".alt-box-1");
+     for (let i = 0; i < nodeList.length; i++) {
+       nodeList[i].style.color = "#5C5C5C";
+     }
+          const nodeList2 = document.querySelectorAll(".alt-box-2");
+     for (let i = 0; i < nodeList2.length; i++) {
+       nodeList2[i].style.color = "#DBD7D7";
+     }
+          const nodeList3 = document.querySelectorAll(".alt-box-3-yazi");
+     for (let i = 0; i < nodeList3.length; i++) {
+       nodeList3[i].style.color = "#ffff";
+     }
+     document.querySelector(".alt-boxa").style.background= "#1B1A1D";
      document.querySelector(".moon").style.transform = "rotate(-24deg)";
      document.querySelector(".sun").style.transform = "rotate(-260deg)";
      document.querySelector(".yazi").style.color = "#ffff";
